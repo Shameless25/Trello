c6736ed12d52b2996cdf3f32afb51a6ee99f0d13
Kajetan Skarubowicz <kajetanskarubowicz@gmail.com>
2024-12-09 21:14:32 +0100

    Mocha and Chai added with used  assertions

--- src/test/test.spec.js
+++ src/test/test.spec.js
@@ -1,7 +1,10 @@
 const LoginPage = require ('./../po/pages/login.page.js');
 const AccountPage = require ('./../po/pages/account.page.js');
 const TablePage = require ('./../po/pages/table.page.js');
-const { expect, browser } = require('@wdio/globals')
+const chai = require('chai');
+const { assert } = chai;
+const { expect } = chai;
+const should = chai.should();
 const loginPage = new LoginPage();
 const accountPage = new AccountPage();
 const tablePage = new TablePage();
@@ -13,28 +16,30 @@ describe('Trello site gnereal test', () => {
         const holder = await loginPage.browserURL();
         await holder.waitForExist({ timeout: 10000 });
         const currentURL = await browser.getUrl();
-        await expect(currentURL).toContain('boards');
+        assert.include(currentURL, 'boards', 'You have been logged in');
     });
 
     it('UC-2: User edits their user profile', async () => {
         await accountPage.goToProfile();
-        await accountPage.changeProfileName('new_user12');
+        await accountPage.changeProfileName('new_user13');
         const alertEl = await accountPage.isAlertVisable();
-        await expect(alertEl).toBeDisplayed();
+        await alertEl.waitForDisplayed({ timeout: 5000 });
+        const isDisplayed = await alertEl.isDisplayed();
+        expect(isDisplayed).to.be.true;
     });
 
     it('UC-3: User can create a board', async () => {
         await accountPage.goToProfile();
         await accountPage.createBoard('Test Board');
         const title = await tablePage.getBoardTitle();
-        await expect(title).toContain('Test Board');
+        title.should.contain('Test Board');
     });
 
     it('UC-4: User can search for a board', async () => {
         await accountPage.goToProfile();
         await accountPage.searchBoard('Test Board');
         const boardVisability = await accountPage.isBoardVisible('Test Board');
-        await expect(boardVisability).toBeDisplayed();
+        expect(await boardVisability.isDisplayed()).to.be.true;
     });
 
     it('UC-5: User can create a list', async () => {
@@ -42,7 +47,7 @@ describe('Trello site gnereal test', () => {
         await accountPage.openBoard('Test Board');
         await tablePage.createList('New List');
         const listTitle = await tablePage.getListTitle('New List')
-        await expect(listTitle).toBe('New List');
+        listTitle.should.equal('New List');
     });
 
     it('UC-6: User can create a card in the list', async () => {
@@ -50,7 +55,7 @@ describe('Trello site gnereal test', () => {
         await accountPage.openBoard('Test Board');
         await tablePage.createCard('New List', 'New Card');
         const cardTitle = await tablePage.getCardTitle('New Card');
-        await expect(cardTitle).toBe('New Card');
+        assert.equal(cardTitle, 'New Card', 'You created new card in the list');
     });
 
     it('UC-7: User can filter cards in a list', async () => {
@@ -60,14 +65,14 @@ describe('Trello site gnereal test', () => {
         await tablePage.filterCards('Filter');
         const filter = await tablePage.getFilteredCardTitle('Filter Card');
         await filter.waitForExist({ timeout:2500 });
-        await expect(filter).toBeDisplayed();
+        expect(await filter.isDisplayed()).to.be.true;
     });
 
     it('UC-8: User can edit their workspace', async () => {
         await accountPage.goToProfile();
         await accountPage.editWorkspaceName('New Workspace Name');
         const workspaceName = await accountPage.getWorkspaceName();
-        await expect(workspaceName).toBe('New Workspace Name');
+        assert.equal(workspaceName, 'New Workspace Name', 'Workspace name is changed');
     });
 })
 
